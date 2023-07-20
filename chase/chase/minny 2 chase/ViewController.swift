cd9de5140dcb3b26ab6516601aaa830e47c98939
ChaseSWL <140095926+ChaseSWL@users.noreply.github.com>
2023-07-20 12:19:48 -0400

    Add files via upload

---
+++ chase/chase/minny 2 chase/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  minny 2 chase
+//
+//  Created by SMART Scholars on 7/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var answerDisplay: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func responce1(_ sender: Any) {
+    }
+    
+    @IBAction func responce2(_ sender: Any) {
+    }
+}
+
---
+++ chase/chase/minny/me/SWC/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  me
+//
+//  Created by SMART Scholars on 7/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var fact1: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
